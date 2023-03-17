91dd676e4a0c8012f2537752592cd5ecf0cf7552
babacar ndiaye <babacar.ndiaye20@uvs.edu.sn>
2023-03-17 13:47:54 +0000

    Maintenant sur login test le web driver peut s'ouvrir sur n'importe quel navigateur il suffit juste de changer le nom du browser en donnant le nom du navigateur choisi et ça pass

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -1,9 +1,52 @@
 package ui;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LoginTest {
+	
+	public static String browser ="chrome";
+	public static WebDriver driver;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		if(browser.equals("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}else if(browser.equals("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--remote-allow-origins=*");
+			options.addArguments("--disable notifications");
+			DesiredCapabilities cp = new DesiredCapabilities();
+			cp.setCapability(ChromeOptions.CAPABILITY, options);
+			options.merge(cp);
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver(options);
+			
+			//WebDriverManager.chromedriver().setup();
+			//driver = new ChromeDriver();
+		}else if(browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+	
+		
+			
+	
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+		
+		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+		
+		
 
 	}
 
